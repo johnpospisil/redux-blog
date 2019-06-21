b46f034901c32d61c36455162c373225d348f494
johnpospisil <audiofreak7@gmail.com>
2019-06-21 10:56:02 -0700

    Refactor 'fetchPostsAndUsers' using Lodash chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPostsAndUsers = () => async dispatch => {
-  console.log('About to fetch posts...');
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  console.log('Fetched posts!');
+
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  //  Below is a refactor for the above code using Lodash;
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value()
 };
 
 export const fetchPosts = () => async dispatch => {
